82fcff05ea9898c42856fd47e8c2f739adfba339
13carlosmata <cmata@kth.se>
2018-01-17 12:26:31 +0100

    +images

--- testingAngular/public_html/app.js
+++ testingAngular/public_html/app.js
@@ -6,18 +6,30 @@
     });
     var gems = [
         {
-            name:'Dodecahedron',
+            name:'Uno',
             price: 2.95,
             description: 'mae uno',
             button1: true,
-            sold:true
+            sold:true,
+            images: [
+                {
+                    full:'images/kth_logo_full.png',
+                    thumb:'images/KTH_logo_thumb.png'
+                },
+            ]
         },
         {
-            name:"pentag gem",
+            name:"Dos",
             price:5.95,
             description:'mae dos',
-            button1:false,
+            button1:true,
             sold:false,
+            images: [
+                {
+                    thumb:'images/kth_logo_full.png',
+                    full:'images/KTH_logo_thumb.png'
+                },
+            ]
         }
     ]
     
